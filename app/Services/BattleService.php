acb5941adda3a44ef91bf0a1725d10aa7e85ead7
vicky <pratamavr@gmail.com>
2026-04-16 15:51:53 +0800

    updates

--- app/Services/BattleService.php
+++ app/Services/BattleService.php
@@ -91,6 +91,7 @@ class BattleService
       $progress->total_wins++;
       $expGained = $rewards['exp'];
       $goldGained = $rewards['gold'];
+      $history->gold_gained = $goldGained;
       $progress->addExp($expGained);
       $history->exp_gained = $expGained;
       $currency->addGold($goldGained);
@@ -107,13 +108,13 @@ class BattleService
       $progress->total_losses++;
       $expGained = max(5, (int)($rewards['exp'] * 0.2));
       $goldGained = max(1, (int)($rewards['gold'] * 0.2));
+      $history->gold_gained = $goldGained;
       $progress->addExp($expGained);
       $history->exp_gained = $expGained;
       $currency->addGold($goldGained);
     }
 
     $progress->save();
-    $history->gold_gained = $goldGained;
     $history->save();
 
     // Cek apakah user naik level
