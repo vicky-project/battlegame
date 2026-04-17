2e27a24097e947edef0b742954164d2f2563e19b
vicky <pratamavr@gmail.com>
2026-04-17 19:05:50 +0800

    updates

---
+++ app/Characters/Enemies/Voidlord.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Modules\BattleGame\Characters\Enemies;
+
+use Modules\BattleGame\Characters\Base\Enemy;
+use Modules\BattleGame\Enums\SkillType;
+
+class VoidLord extends Enemy
+{
+  public function __construct() {
+    parent::__construct(
+      id: 'void_lord',
+      name: 'Void Lord',
+      emoji: '🌑👑',
+      minLevel: 30,
+      maxLevel: null,
+      rewards: ['exp' => 500, 'gold' => 400],
+    );
+  }
+
+  public function baseHp(): int {
+    return 1200;
+  }
+  public function baseAtk(): int {
+    return 120;
+  }
+  public function baseDef(): int {
+    return 50;
+  }
+  public function baseAspd(): float {
+    return 3.5;
+  }
+  public function baseBlockChance(): float {
+    return 0.4;
+  }
+  public function baseBlockReduction(): float {
+    return 0.6;
+  }
+  public function baseAccuracy(): float {
+    return 0.98;
+  }
+  public function baseEvasion(): float {
+    return 0.10;
+  }
+  public function levelScalingFactor(): float {
+    return 0.3;
+  }
+
+  public function specialAbility(): array
+  {
+    // Void Lord memiliki kombinasi skill mematikan
+    return [
+      'type' => SkillType::LIFESTEAL,
+      // Bisa juga kita buat multi-skill, tapi untuk simplicity pilih satu
+      'chance' => 1.0,
+      'value' => 0.5,
+      // 50% lifesteal
+    ];
+  }
+}
