15e6757860c17fd469fa9053667922f26532fb3c
vicky <pratamavr@gmail.com>
2026-04-18 23:44:46 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -103,10 +103,10 @@
   <span class="badge bg-secondary" style="cursor:pointer;" id="btn-help-bar"><i class="bi bi-question-circle"></i></span>
   </div>
   <div>
-  <span class="badge bg-warning text-dark me-2" style="cursor:pointer;" id="btn-gold-bar"><i class="bi bi-coin"></i> ${state.storeData.gold}</span>
-  <span class="badge bg-info text-dark me-2" style="cursor:pointer;" id="btn-diamond-bar"><i class="bi bi-gem"></i> ${state.storeData.diamond}</span>
-  <span class="badge me-2" style="cursor:pointer;" id="btn-store-bar">🛒</span>
-  <span class="badge bg-success me-2" style="cursor:pointer;" id="btn-history-bar"><i class="bi bi-clock-history"></i></span>
+  <span class="badge bg-warning text-dark me-1" style="cursor:pointer;" id="btn-gold-bar"><i class="bi bi-coin"></i> ${state.storeData.gold}</span>
+  <span class="badge bg-info text-dark me-1" style="cursor:pointer;" id="btn-diamond-bar"><i class="bi bi-gem"></i> ${state.storeData.diamond}</span>
+  <span class="badge bg-light me-1" style="cursor:pointer;" id="btn-store-bar">🛒</span>
+  <span class="badge bg-success" style="cursor:pointer;" id="btn-history-bar"><i class="bi bi-clock-history"></i></span>
   </div>
   </div>
   `;
