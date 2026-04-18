da230140273e50e6d9e5bb8467d4c5b733ae1157
vicky <pratamavr@gmail.com>
2026-04-18 20:37:25 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -290,7 +290,7 @@
   <div class="d-flex">
   <!-- Emoji -->
   <div class="text-center me-3">
-  ${renderLayeredEmoji(h.emoji || '👤', 48)}
+  ${renderLayeredEmoji(h.emoji || '👤', 60)}
   </div>
   <!-- Nama, Level, Stats Dasar -->
   <div class="flex-grow-1">
