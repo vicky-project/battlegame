0b860bac6d6716a1ab91932f0340a7ae3ca4a22e
vicky <pratamavr@gmail.com>
2026-04-18 16:43:04 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -284,7 +284,7 @@
   <div class="col-6">
   <div class="card">
   <div class="card-body text-center">
-  <span style="font-size:48px;">${r.player_emoji}</span>
+  <span style="font-size:48px;">${renderLayeredEmoji(r.player_emoji)}</span>
   <h5>${r.player_name}</h5>
   <div class="d-flex justify-content-center align-items-center gap-2 small">
   <span class="badge bg-secondary">Lv.${state.battleResult.player_level || '?'}</span>
