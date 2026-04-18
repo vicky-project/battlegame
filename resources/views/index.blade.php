af78584b27ad14725cc3c6975c96696be56f4b23
vicky <pratamavr@gmail.com>
2026-04-19 02:29:36 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -451,7 +451,7 @@
   </div>
   <div class="d-grid gap-2 mb-3">
   <button class="btn btn-primary" id="btn-battle-again">Bertarung Lagi (<i class="bi bi-coin"></i>${BATTLE_COST})</button>
-  <button class="btn btn-outline-info" id="btn-replay-battle"><i class="bi bi-play-circle me-2"></i>Replay</button>
+  <button class="btn btn-outline-info" id="btn-replay-battle"><i class="bi bi-play-circle me-1"></i>Replay</button>
   <button class="btn btn-outline-secondary" id="btn-back-home-from-result">Beranda</button>
   </div>
   <div class="card">
